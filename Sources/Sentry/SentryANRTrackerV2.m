fe6c6d62536347b17c4dab44077ff9ffd478e22a
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-08-09 08:18:27 +0200

    backup

---
+++ Sources/Sentry/SentryANRTrackerV2.m
@@ -0,0 +1,235 @@
+#import "SentryANRTrackerV2.h"
+#import "SentryCrashWrapper.h"
+#import "SentryDependencyContainer.h"
+#import "SentryDispatchQueueWrapper.h"
+#import "SentryFramesTracker.h"
+#import "SentryLog.h"
+#import "SentrySwift.h"
+#import "SentryThreadWrapper.h"
+#import "SentryTime.h"
+#import <stdatomic.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, SentryANRTrackerState) {
+    kSentryANRTrackerNotRunning = 1,
+    kSentryANRTrackerRunning,
+    kSentryANRTrackerStarting,
+    kSentryANRTrackerStopping
+};
+
+@interface
+SentryANRTrackerV2 ()
+
+@property (nonatomic, strong) SentryCrashWrapper *crashWrapper;
+@property (nonatomic, strong) SentryDispatchQueueWrapper *dispatchQueueWrapper;
+@property (nonatomic, strong) SentryThreadWrapper *threadWrapper;
+@property (nonatomic, strong) NSHashTable<id<SentryANRTrackerV2Delegate>> *listeners;
+@property (nonatomic, strong) SentryFramesTracker *framesTracker;
+@property (nonatomic, assign) NSTimeInterval timeoutInterval;
+
+@end
+
+@implementation SentryANRTrackerV2 {
+    NSObject *threadLock;
+    SentryANRTrackerState state;
+}
+
+- (instancetype)initWithTimeoutInterval:(NSTimeInterval)timeoutInterval
+                           crashWrapper:(SentryCrashWrapper *)crashWrapper
+                   dispatchQueueWrapper:(SentryDispatchQueueWrapper *)dispatchQueueWrapper
+                          threadWrapper:(SentryThreadWrapper *)threadWrapper
+                          framesTracker:(SentryFramesTracker *)framesTracker
+{
+    if (self = [super init]) {
+        self.timeoutInterval = timeoutInterval;
+        self.crashWrapper = crashWrapper;
+        self.dispatchQueueWrapper = dispatchQueueWrapper;
+        self.threadWrapper = threadWrapper;
+        self.framesTracker = framesTracker;
+        self.listeners = [NSHashTable weakObjectsHashTable];
+        threadLock = [[NSObject alloc] init];
+        state = kSentryANRTrackerNotRunning;
+    }
+    return self;
+}
+
+- (void)detectANRs
+{
+    NSUUID *threadID = [NSUUID UUID];
+
+    @synchronized(threadLock) {
+        [self.threadWrapper threadStarted:threadID];
+
+        if (state != kSentryANRTrackerStarting) {
+            [self.threadWrapper threadFinished:threadID];
+            return;
+        }
+
+        NSThread.currentThread.name = @"io.sentry.app-hang-tracker";
+        state = kSentryANRTrackerRunning;
+    }
+
+    BOOL reported = NO;
+
+    NSInteger reportThreshold = 5;
+    NSTimeInterval sleepInterval = self.timeoutInterval / reportThreshold;
+
+    SentryCurrentDateProvider *dateProvider = SentryDependencyContainer.sharedInstance.dateProvider;
+
+    // Canceling the thread can take up to sleepInterval.
+    while (YES) {
+        @synchronized(threadLock) {
+            if (state != kSentryANRTrackerRunning) {
+                break;
+            }
+        }
+
+        NSDate *blockDeadline = [[dateProvider date] dateByAddingTimeInterval:self.timeoutInterval];
+
+        [self.threadWrapper sleepForTimeInterval:sleepInterval];
+
+        if (![self.crashWrapper isApplicationInForeground]) {
+            SENTRY_LOG_DEBUG(@"Ignoring potential app hangs because the app is in the background");
+            continue;
+        }
+
+        // The blockDeadline should be roughly executed after the timeoutInterval even if there is
+        // an ANR. If the app gets suspended this thread could sleep and wake up again. To avoid
+        // false positives, we don't report AppHangs if the delta is too big.
+        NSTimeInterval deltaFromNowToBlockDeadline =
+            [[dateProvider date] timeIntervalSinceDate:blockDeadline];
+
+        if (deltaFromNowToBlockDeadline >= self.timeoutInterval) {
+            SENTRY_LOG_DEBUG(
+                @"Ignoring ANR because the delta is too big: %f.", deltaFromNowToBlockDeadline);
+            continue;
+        }
+
+        uint64_t frameDelayEndSystemTime = dateProvider.systemTime;
+
+        uint64_t frameDelayStartSystemTime
+            = frameDelayEndSystemTime - timeIntervalToNanoseconds(self.timeoutInterval);
+
+        CFTimeInterval framesDelayForTimeInterval =
+            [self.framesTracker getFramesDelay:frameDelayStartSystemTime
+                            endSystemTimestamp:frameDelayEndSystemTime];
+
+        uint64_t sleepIntervalStartedSystemTime
+            = frameDelayEndSystemTime - timeIntervalToNanoseconds(sleepInterval);
+
+        CFTimeInterval framesDelayForThisSleepInterval =
+            [self.framesTracker getFramesDelay:sleepIntervalStartedSystemTime
+                            endSystemTimestamp:frameDelayEndSystemTime];
+
+        if (framesDelayForThisSleepInterval < sleepInterval * 0.3) {
+
+            if (reported) {
+                SENTRY_LOG_DEBUG(@"App hang stopped.");
+
+                // The App Hang stopped, don't block the App Hangs thread or the main thread with
+                // calling ANRStopped listeners.
+                [self.dispatchQueueWrapper dispatchAsyncWithBlock:^{ [self ANRStopped]; }];
+            }
+
+            reported = NO;
+        }
+
+        NSTimeInterval nonFullyBlockingFramesDelayThreshold = self.timeoutInterval * 0.9;
+
+        if (framesDelayForTimeInterval < nonFullyBlockingFramesDelayThreshold) {
+            continue;
+        }
+
+        if (fabs(framesDelayForTimeInterval - self.timeoutInterval) < 0.001 && !reported) {
+            reported = YES;
+
+            SENTRY_LOG_WARN(@"App Hang detected: fully-blocking.");
+            [self ANRDetected:kSentryANRTypeFullyBlocking];
+        } else if (framesDelayForTimeInterval > nonFullyBlockingFramesDelayThreshold && !reported) {
+
+            reported = YES;
+
+            SENTRY_LOG_WARN(@"App Hang detected: non-fully-blocking.");
+            [self ANRDetected:kSentryANRTypeNonFullyBlocking];
+        }
+    }
+
+    @synchronized(threadLock) {
+        state = kSentryANRTrackerNotRunning;
+        [self.threadWrapper threadFinished:threadID];
+    }
+}
+
+- (void)ANRDetected:(SentryANRType)type
+{
+    NSArray *localListeners;
+    @synchronized(self.listeners) {
+        localListeners = [self.listeners allObjects];
+    }
+
+    for (id<SentryANRTrackerV2Delegate> target in localListeners) {
+        [target anrDetected:type];
+    }
+}
+
+- (void)ANRStopped
+{
+    NSArray *targets;
+    @synchronized(self.listeners) {
+        targets = [self.listeners allObjects];
+    }
+
+    for (id<SentryANRTrackerV2Delegate> target in targets) {
+        [target anrStopped];
+    }
+}
+
+- (void)addListener:(id<SentryANRTrackerV2Delegate>)listener
+{
+    @synchronized(self.listeners) {
+        [self.listeners addObject:listener];
+
+        @synchronized(threadLock) {
+            if (self.listeners.count > 0 && state == kSentryANRTrackerNotRunning) {
+                if (state == kSentryANRTrackerNotRunning) {
+                    state = kSentryANRTrackerStarting;
+                    [NSThread detachNewThreadSelector:@selector(detectANRs)
+                                             toTarget:self
+                                           withObject:nil];
+                }
+            }
+        }
+    }
+}
+
+- (void)removeListener:(id<SentryANRTrackerV2Delegate>)listener
+{
+    @synchronized(self.listeners) {
+        [self.listeners removeObject:listener];
+
+        if (self.listeners.count == 0) {
+            [self stop];
+        }
+    }
+}
+
+- (void)clear
+{
+    @synchronized(self.listeners) {
+        [self.listeners removeAllObjects];
+        [self stop];
+    }
+}
+
+- (void)stop
+{
+    @synchronized(threadLock) {
+        SENTRY_LOG_INFO(@"Stopping App Hang detection");
+        state = kSentryANRTrackerStopping;
+    }
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tests/SentryTests/Integrations/ANR/SentryANRTrackerV2Tests.swift
@@ -0,0 +1,357 @@
+@testable import Sentry
+import SentryTestUtils
+import XCTest
+
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+class SentryANRTrackerV2Tests: XCTestCase {
+    
+    private let waitTimeout: TimeInterval = 1.0
+    private let timeoutInterval: TimeInterval = 2
+        
+    private func getSut() throws -> (SentryANRTrackerV2, TestCurrentDateProvider, TestDisplayLinkWrapper, TestSentryCrashWrapper, SentryTestThreadWrapper) {
+        
+        let currentDate = TestCurrentDateProvider()
+        let crashWrapper = TestSentryCrashWrapper.sharedInstance()
+        let dispatchQueue = TestSentryDispatchQueueWrapper()
+        let threadWrapper = SentryTestThreadWrapper()
+        let displayLinkWrapper = TestDisplayLinkWrapper()
+        
+        SentryDependencyContainer.sharedInstance().dateProvider = currentDate
+        
+        displayLinkWrapper.dateProvider = currentDate
+        let framesTracker = SentryFramesTracker(displayLinkWrapper: displayLinkWrapper, dateProvider: currentDate, dispatchQueueWrapper: dispatchQueue, notificationCenter: TestNSNotificationCenterWrapper(), keepDelayedFramesDuration: 30)
+        
+        framesTracker.start()
+        
+        // Add a couple of normal frames, so that querying for frame delay
+        // has enough data to not return -1.
+        for _ in 0..<1_000 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        return (SentryANRTrackerV2(
+            timeoutInterval: timeoutInterval,
+            crashWrapper: crashWrapper,
+            dispatchQueueWrapper: dispatchQueue,
+            threadWrapper: threadWrapper,
+            framesTracker: framesTracker), currentDate, displayLinkWrapper, crashWrapper, threadWrapper)
+    }
+    
+    override func setUp() {
+        super.setUp()
+        
+        // To avoid spamming the test logs
+        SentryLog.configure(true, diagnosticLevel: .error)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        SentryLog.setTestDefaultLogLevel()
+    }
+    
+    func testNonFullyBlockingContinuousANRs_OneReported() throws {
+        let (sut, _, displayLinkWrapper, _, _) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate()
+        
+        sut.addListener(listener)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        displayLinkWrapper.frameWith(delay: 1.81)
+        
+        wait(for: [listener.anrDetectedExpectation], timeout: waitTimeout)
+        XCTAssertEqual(SentryANRType.nonFullyBlocking, listener.lastANRDetectedType)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        wait(for: [listener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testAlmostNonFullyBlockingAppHang_NoneReported() throws {
+        let (sut, _, displayLinkWrapper, _, _) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        sut.addListener(listener)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        displayLinkWrapper.frameWith(delay: 1.8)
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testFullyBlockingContinuousANRs_OneReported() throws {
+        let (sut, _, displayLinkWrapper, _, _) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate()
+        
+        sut.addListener(listener)
+        
+        displayLinkWrapper.frameWith(delay: timeoutInterval * 2.0)
+        
+        wait(for: [listener.anrDetectedExpectation], timeout: waitTimeout)
+        XCTAssertEqual(SentryANRType.fullyBlocking, listener.lastANRDetectedType)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        wait(for: [listener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testMultipleListeners() throws {
+        let (sut, _, displayLinkWrapper, _, _) = try getSut()
+        defer { sut.clear() }
+        
+        let firstListener = SentryANRTrackerV2TestDelegate()
+        
+        sut.addListener(firstListener)
+        
+        let secondListener = SentryANRTrackerV2TestDelegate()
+        sut.addListener(secondListener)
+        
+        displayLinkWrapper.frameWith(delay: timeoutInterval * 2.0)
+        
+        wait(for: [firstListener.anrDetectedExpectation, secondListener.anrDetectedExpectation], timeout: waitTimeout)
+        
+        XCTAssertEqual(SentryANRType.fullyBlocking, firstListener.lastANRDetectedType)
+        XCTAssertEqual(SentryANRType.fullyBlocking, secondListener.lastANRDetectedType)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        wait(for: [firstListener.anrStoppedExpectation, secondListener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testFullyBlockingANR_ButAppInBackground_NoANRReported() throws {
+        let (sut, _, displayLinkWrapper, crashWrapper, _) = try getSut()
+        defer { sut.clear() }
+        
+        crashWrapper.internalIsApplicationInForeground = false
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        sut.addListener(listener)
+        
+        displayLinkWrapper.frameWith(delay: timeoutInterval * 2.0)
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testNonFullyBlockingANR_ButAppInBackground_NoANRReported() throws {
+        let (sut, _, displayLinkWrapper, crashWrapper, _) = try getSut()
+        defer { sut.clear() }
+        
+        crashWrapper.internalIsApplicationInForeground = false
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        sut.addListener(listener)
+        
+        for _ in 0..<10 {
+            _ = displayLinkWrapper.fastestFrozenFrame()
+        }
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testMultipleANRs_MultipleReported() throws {
+        let (sut, _, displayLinkWrapper, _, _) = try getSut()
+        defer { sut.clear() }
+        
+        let firstListener = SentryANRTrackerV2TestDelegate()
+        sut.addListener(firstListener)
+    
+        for _ in 0..<10 {
+            _ = displayLinkWrapper.fastestFrozenFrame()
+        }
+        
+        wait(for: [firstListener.anrDetectedExpectation], timeout: waitTimeout)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        wait(for: [firstListener.anrStoppedExpectation], timeout: waitTimeout)
+        
+        let secondListener = SentryANRTrackerV2TestDelegate()
+        
+        sut.addListener(secondListener)
+        
+        for _ in 0..<10 {
+            _ = displayLinkWrapper.fastestFrozenFrame()
+        }
+        
+        wait(for: [secondListener.anrDetectedExpectation], timeout: waitTimeout)
+        
+        for _ in 0..<100 {
+            displayLinkWrapper.normalFrame()
+        }
+        
+        wait(for: [secondListener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testAppSuspended_NoANR() throws {
+        let (sut, currentDate, displayLinkWrapper, _, threadWrapper) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        for _ in 0..<10 {
+            _ = displayLinkWrapper.fastestFrozenFrame()
+        }
+        
+        threadWrapper.blockWhenSleeping = {
+            let delta = self.timeoutInterval * 2
+            
+            currentDate.setDate(date: currentDate.date().addingTimeInterval(delta))
+        }
+        
+        sut.addListener(listener)
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation], timeout: waitTimeout)
+    }
+    
+    func testRemoveListener_StopsReportingANRs() throws {
+        let (sut, _, displayLinkWrapper, _, threadWrapper) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        let mainBlockExpectation = expectation(description: "Main Block")
+        
+        threadWrapper.blockWhenSleeping = {
+            sut.removeListener(listener)
+            mainBlockExpectation.fulfill()
+        }
+        
+        displayLinkWrapper.frameWith(delay: 10.0)
+        
+        sut.addListener(listener)
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation, mainBlockExpectation], timeout: waitTimeout)
+    }
+    
+    func testClear_StopsReportingANRs() throws {
+        let (sut, _, _, _, threadWrapper) = try getSut()
+        defer { sut.clear() }
+        
+        let firstListener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        let secondListener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        let mainBlockExpectation = expectation(description: "Main Block")
+        
+        //Having a second Listener may cause the tracker to execute more than once before the end of the test
+        mainBlockExpectation.assertForOverFulfill = false
+        
+        threadWrapper.blockWhenSleeping = {
+            sut.clear()
+            mainBlockExpectation.fulfill()
+        }
+        
+        sut.addListener(secondListener)
+        sut.addListener(firstListener)
+        
+        wait(for: [firstListener.anrDetectedExpectation, firstListener.anrStoppedExpectation, mainBlockExpectation, secondListener.anrStoppedExpectation, secondListener.anrDetectedExpectation], timeout: waitTimeout)
+    }
+    
+    func testNotRemovingDeallocatedListener_DoesNotRetainListener_AndStopsTracking() throws {
+        let (sut, _, _, _, _) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        // So ARC deallocates SentryANRTrackerTestDelegate
+        let addListenersCount = 10
+        func addListeners() {
+            for _ in 0..<addListenersCount {
+                sut.addListener(SentryANRTrackerV2TestDelegate())
+            }
+        }
+        addListeners()
+        
+        sut.addListener(listener)
+        sut.removeListener(listener)
+        
+        let listeners = Dynamic(sut).listeners.asObject as? NSHashTable<NSObject>
+        
+        XCTAssertGreaterThan(addListenersCount, listeners?.count ?? addListenersCount)
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation], timeout: self.waitTimeout)
+    }
+    
+    func testClearStopsThread() throws {
+        let (sut, _, _, _, threadWrapper) = try getSut()
+        
+        sut.addListener(SentryANRTrackerV2TestDelegate())
+        
+        sut.clear()
+        
+        wait(for: [threadWrapper.threadFinishedExpectation], timeout: 5)
+        XCTAssertEqual(0, threadWrapper.threads.count)
+    }
+    
+    func testClearDirectlyAfterStart() throws {
+        let (sut, _, displayLinkWrapper, _, threadWrapper) = try getSut()
+        defer { sut.clear() }
+        
+        let listener = SentryANRTrackerV2TestDelegate(shouldANRBeDetected: false, shouldStoppedBeCalled: false)
+        
+        displayLinkWrapper.frameWith(delay: timeoutInterval * 2)
+        
+        let invocations = 10
+        for _ in 0..<invocations {
+            sut.addListener(listener)
+            sut.clear()
+        }
+        
+        wait(for: [listener.anrDetectedExpectation, listener.anrStoppedExpectation], timeout: self.waitTimeout)
+        
+        XCTAssertEqual(0, threadWrapper.threads.count)
+        XCTAssertEqual(1, threadWrapper.threadStartedInvocations.count)
+        XCTAssertEqual(1, threadWrapper.threadFinishedInvocations.count)
+    }
+}
+
+class SentryANRTrackerV2TestDelegate: NSObject, SentryANRTrackerV2Delegate {
+    
+    let anrDetectedExpectation = XCTestExpectation(description: "Test Delegate ANR Detection")
+    let anrStoppedExpectation  = XCTestExpectation(description: "Test Delegate ANR Stopped")
+    
+    var lastANRDetectedType: SentryANRType?
+    
+    init(shouldANRBeDetected: Bool = true, shouldStoppedBeCalled: Bool = true) {
+        if !shouldANRBeDetected {
+            anrDetectedExpectation.isInverted = true
+        }
+        
+        if !shouldStoppedBeCalled {
+            anrStoppedExpectation.isInverted = true
+        }
+    }
+    
+    func anrStopped() {
+        anrStoppedExpectation.fulfill()
+    }
+    
+    func anrDetected(_ type: SentryANRType) {
+        anrDetectedExpectation.fulfill()
+        lastANRDetectedType = type
+    }
+}
+
+#endif
